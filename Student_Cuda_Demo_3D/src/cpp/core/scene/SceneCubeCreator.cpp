c4fc4944461fbd8b75e7389841ed0199fc22600e
= <axel.rieben@he-arc.ch>
2018-04-09 17:24:42 +0200

    Add project demo 3D

---
+++ Student_Cuda_Demo_3D/src/cpp/core/scene/SceneCubeCreator.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+#include "SceneCubeCreator.h"
+
+#include "RipplingProvider.h"
+
+
+/*----------------------------------------------------------------------*\
+ |*			Declaration 					*|
+ \*---------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------*\
+ |*			Implementation 					*|
+ \*---------------------------------------------------------------------*/
+
+/*--------------------------------------*\
+ |*		Constructor		*|
+ \*-------------------------------------*/
+
+SceneCubeCreator::SceneCubeCreator()
+    {
+    createImages();
+    createScene();
+    }
+
+SceneCubeCreator::~SceneCubeCreator()
+    {
+    delete ptrImage1;
+    delete ptrImage2;
+    delete ptrImage3;
+    delete ptrImage4;
+    delete ptrImage5;
+    delete ptrImage6;
+
+    delete ptrScene;
+    }
+
+/*--------------------------------------*\
+ |*		Public			*|
+ \*-------------------------------------*/
+
+SceneCube* SceneCubeCreator::getScene()
+    {
+    return ptrScene;
+    }
+
+/*--------------------------------------*\
+ |*		Private			*|
+ \*-------------------------------------*/
+
+void SceneCubeCreator::createImages()
+    {
+    ptrImage1 = RipplingProvider().createImageGL();
+    ptrImage2 = RipplingProvider().createImageGL();
+
+    ptrImage3 = RipplingProvider().createImageGL();
+    ptrImage4 = RipplingProvider().createImageGL();
+    ptrImage5 = RipplingProvider().createImageGL(); //ko, FIXME JST shader
+    ptrImage6 = RipplingProvider().createImageGL(); // ko, FIXME JST shader
+    }
+
+void SceneCubeCreator::createScene()
+    {
+    ptrScene = new SceneCube(ptrImage1, ptrImage2, ptrImage3, ptrImage4, ptrImage5, ptrImage6);
+    }
+
+/*----------------------------------------------------------------------*\
+ |*			End	 					*|
+ \*---------------------------------------------------------------------*/
+
